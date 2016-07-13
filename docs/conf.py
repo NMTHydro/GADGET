c0e0b803fcfc48ba17a6b238ed39cfb4280b3e6e
Jake Ross <jirhiker@gmail.com>
2016-07-13 13:00:07 -0600

    setup rtd

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,12 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {
+    '.md': CommonMarkParser
+}
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
 
